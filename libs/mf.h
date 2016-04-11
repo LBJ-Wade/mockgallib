38b0320223b0721cb65020c80a21c308e24cae23
Jun Koda <jun.koda@brera.inaf.it>
2016-04-11 22:02:58 +0200

    Add mass function module mf.

---
+++ libs/mf.h
@@ -0,0 +1,17 @@
+#ifndef TINKER_H
+#define TINKER_H 1
+
+struct MF {
+  double z;
+  double alpha;
+  gsl_integration_cquad_workspace* w;
+};
+
+MF* mf_alloc();
+void mf_free(MF* const mf);
+void mf_set_redshift(MF* const mf, const double a);
+double mf_f(MF const * const mf, const double nu);
+double mf_b(const double nu);
+
+#endif
+
