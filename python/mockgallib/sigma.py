db272f9b8701496a1383dfce4efc04f576220f8b
Jun Koda <jun.koda@brera.inaf.it>
2016-04-13 08:54:25 +0200

    Chenge notation in sigma module; sigma(M), sigma.inv(sigma).

--- python/mockgallib/sigma.py
+++ python/mockgallib/sigma.py
@@ -1,5 +1,13 @@
 import mockgallib._mockgallib as c
 
+# Sigma sigma(PowerSpectrum ps)
+#   Amplitude (redshift) of sigma is that of power spectrum
+#
+# sigma.M       array of Mass [1/h Solar mass]
+# sigma.sinv    array of 1/sigma(M)
+# sigma(M)      sigma function of M (double) -> sigma
+# sigma.inv(s)  inverse function sigma -> M
+
 class Sigma:
     def __init__(self, ps):
         self._s = c._sigma_alloc(ps._ps)
@@ -7,8 +15,8 @@ class Sigma:
         self.M= c._sigma_M_array(self._s)
         self.sinv= c._sigma_sinv_array(self._s)
         self.M_range= c._sigma_M_range(self._s)
-        self.sigma0_range= (self.sigma0(self.M_range[0]),
-                          self.sigma0(self.M_range[1]))
+        self.sigma0_range= (self(self.M_range[0]),
+                            self(self.M_range[1]))
         
     def __len__(self):
         return self.n
@@ -27,8 +35,9 @@ class Sigma:
             str += "% .3e % .3e\n" % (self.M[self.n-1], 1.0/self.sinv[self.n-1])
         return str
 
-    def mass(self, sigma0):
+    def __call__(self, M):
+        return 1.0/c._sigma_0inv(self._s, M)
+
+    def inv(self, sigma0):
         return c._sigma_M(self._s, sigma0)
 
-    def sigma0(self, M):
-        return 1.0/c._sigma_0inv(self._s, M)
