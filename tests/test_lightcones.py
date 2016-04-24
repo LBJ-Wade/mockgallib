318c24d75e0b630e9e114406a038dcd801ba25d9
Jun Koda <jun.koda@brera.inaf.it>
2016-04-24 07:24:43 +0200

    Fix broken test.

--- tests/test_lightcones.py
+++ tests/test_lightcones.py
@@ -6,15 +6,8 @@ class TestLightcones(unittest.TestCase):
         self.lightcones = mock.LightCones()
 
     def test_load_filename(self):
-        self.lightcones.load('../data/lightcone_00001.h5')
+        self.lightcones.load(['../data/halo_lightcone_mini.h5'])
         self.assertEqual(len(self.lightcones), 1)
-        self.lightcones.load('../data/lightcone_00002.h5')
-        self.assertEqual(len(self.lightcones), 2)
-
-    def test_load_filenames(self):
-        filenames = [ "../data/lightcone_%05d.h5" % i for i in range(1,4) ]
-        self.lightcones.load(filenames)
-        self.assertEqual(len(self.lightcones), 3)
 
 
 if __name__ == '__main__':
