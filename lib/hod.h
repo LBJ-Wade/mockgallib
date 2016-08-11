f03ff6934d8fcb4ef8979159604a08f89ce2feb6
Jun Koda <jun.koda@brera.inaf.it>
2016-08-11 16:57:50 +0200

    Change parameterisation of M1, now proportional to M_min.

--- lib/hod.h
+++ lib/hod.h
@@ -14,13 +14,14 @@ struct Hod {
     logMmin= c[0] + (c[1] + (c[2] + c[3]*x)*x)*x;
     sigma=   c[4] + c[5]*x;
     M0=      pow(10.0, logMmin);
-    M1=      pow(10.0, c[6] + c[7]*x);
+    M1=      pow(10.0, logMmin + c[6] + c[7]*x);
     alpha=   c[8] + c[9]*x;
   }
     
   double ncen(const double M) const {
     return 0.5*(1.0 + erf((log10(M) - logMmin)/sigma));
   }
+  
   double nsat(const double M) const {
     return M <= M0 ? 0.0 : pow((M - M0)/M1, alpha);
   }
