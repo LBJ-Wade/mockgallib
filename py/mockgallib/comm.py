2f4e9d227e83ecc135f3822286ca37ed8e97d388
Jun Koda <jun.koda@brera.inaf.it>
2016-08-25 17:36:26 +0200

    Add comm.py.

---
+++ py/mockgallib/comm.py
@@ -0,0 +1,24 @@
+import mockgallib._mockgallib as c
+import atexit
+
+rank= -1
+n_nodes= 0
+
+def init():
+    c._comm_init()
+    atexit.register(c._comm_finalise)
+
+    global rank
+    rank = c._comm_this_rank()
+
+    global n_nodes
+    n_nodes = c._comm_n_nodes()
+
+def bcast_str(src_str):
+    """Broadcast string to other MPI nodes from rank=0
+    Args:
+        src_str (str): sourse sring at rank=0
+    Return value:
+        str:           src_str in all MPI nodes
+    """
+    return c._comm_bcast_str(src_str)
