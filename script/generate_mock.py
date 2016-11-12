23169d2cd32761202b9898bf97418061ccd98cb1
Jun Koda <jun.koda@brera.inaf.it>
2016-11-12 13:13:11 +0100

    Copy from doraemon.

--- script/generate_mock.py
+++ script/generate_mock.py
@@ -16,7 +16,7 @@ Input:
 
 Output:
     mocks/mock_<n>.txt
-    randoms/random_<n>.txt
+    rands/random_<n>.txt
 """
 
 import os
@@ -37,8 +37,11 @@ parser.add_argument('--reg', default='w1', help='region w1/w4')
 parser.add_argument('--dir', default='.', help='base data directory') 
 parser.add_argument('--param', default='param.json',
                     help='parameter json file')
-parser.add_argument('--random', help='generate random catalogue',
+parser.add_argument('--mock', help='generate mock catalogue',
                     action="store_true")
+parser.add_argument('--rand', help='generate random catalogue',
+                    action="store_true")
+
 arg = parser.parse_args()
 
 data_dir = '/workplace/wp2e/como5/data'
@@ -63,67 +66,76 @@ mock.cosmology.set(omega_m)
 mock.power.init(arg.dir + '/' + param['power_spectrum'])
 
 # nz
-nbar_obs=  mock.array.loadtxt(arg.dir + '/' + param['nz'])
+# nbar_obs=  mock.array.loadtxt(arg.dir + '/' + param['nz'])
+
+# sky
+for reg in param['reg']:
+    sky[reg['name']] = mock.Sky(reg['ra'], reg['dec'], [z_min_all, z_max_all])
+
 
 #
 # Set HOD parameters
 #
 hod = mock.Hod()
-hod_param = [11.5659763286151, -1.3653860293894984, 7.372119497149173, -3.905197669231047, 0.4114530081890546, 0.0, 4.480542558267717, 0.0, 1.1909781896845204, 0.0]
+hod_param = [11.68219947743167, -0.32842888873294585, 2.767160089761731, 0.05782377516591075, 0.25, 0.0, 0.7, 6.0, 0.9, 0.0, 3.0, 2.0]
 hod.set_coef(hod_param)
-nbar= mock.NbarFitting(hod, nbar_obs, 0.6, 1.2)
+#nbar= mock.NbarFitting(hod, nbar_obs, 0.6, 1.2)
 
-x0 = [1.0, 0.15, 1.0]
-nbar.fit()
+#x0 = [1.0, 0.15, 1.0]
+#nbar.fit()
     
 lightcones = mock.LightCones()
 cats = mock.Catalogues()
 
 n = int(arg.n)
-domain = ('w1', '0.8', '1.0')
+domain = (arg.reg, '0.5', '1.2')
 z_min = float(domain[1])
 z_max = float(domain[2])
 
-halo_lightcones = mock.LightCones()
-halo_lightcones.load_h5(
-    ['%s/halo_lightcone/%s/lightcone_%05d.h5' % (arg.dir, domain[0], n)])
+def write_catalogue(filename, a):
+    with open(filename, 'w') as f:
+        for i in range(a.shape[0]):
+            f.write('%d %e %e %e %e %e %e %e %e %e %e\n' % (
+                    i,
+                    a[i,0], a[i,1], a[i,2], 
+                    a[i,4], a[i,3],
+                    a[i,5], a[i,6],
+                    a[i,7], a[i,10], a[i,11]))
 
-rand_lightcones = mock.LightCones()
-rand_lightcones.load_h5(
-    ['%s/rand_lightcone/%s/lightcone_%05d.h5' % (arg.dir, domain[0], n)])
+reg = arg.reg
 
-galaxy_catalogues = mock.Catalogues()
-random_catalogues = mock.Catalogues()
+# mock
+if arg.mock:
+    halo_lightcones = mock.LightCones()
+    halo_lightcones.load_h5(
+        ['%s/halo_lightcone/%s/lightcone_%05d.h5' % (arg.dir, domain[0], n)])
+    galaxy_catalogues = mock.Catalogues()
+    galaxy_catalogues.generate_galaxies(hod, halo_lightcones, sky[reg],
+                                        z_min, z_max)
 
-wp_obs = mock.array.loadtxt(
-    '%s/vipers/run4/0.1/corr_projected_%s_%s_%s.txt' % ((data_dir,) + domain))
-        
-covinv = np.load('%s/vipers/run4/0.1/covinv_%s_%s_%s.npy' %
-                 ((data_dir,) + domain))
+    write_catalogue('mocks/%s/mock_%s_%05d.txt' % (reg, reg, n), 
+                    galaxy_catalogues[0])
 
-galaxy_catalogues.generate_galaxies(hod, halo_lightcones, z_min, z_max)
-random_catalogues.generate_randoms(hod,  rand_lightcones, z_min, z_max)
+if arg.rand:
+    rand_lightcones = mock.LightCones()
+    rand_lightcones.load_h5(
+        ['%s/rand_lightcone/%s/lightcone_%05d.h5' % (arg.dir, domain[0], n)])
 
+    random_catalogues = mock.Catalogues()
+    random_catalogues.generate_randoms(hod,  rand_lightcones, sky[reg],
+                                       z_min, z_max)
 
+    write_catalogue('rands/%s/rand_%s_%05d.txt' % (reg, reg, n), 
+                    random_catalogues[0])
 
-with open('mock.txt', 'w') as f:
-    a = galaxy_catalogues[0]
-    print(a.shape)
-    for i in range(a.shape[0]):
-        f.write('%e %e %e %e %e %e\n' % (a[i,0], a[i,1], a[i,2], a[i,3],
-                                   a[i,7], a[i,10]))
-
-    
-# Column 1: x
+# Column 0: index
+# Column 1: x realspace [1/h Mpc]
 # Column 2: y
 # Column 3: z
-# Column 4: redshift
-# Column 5: M
-# Column 6: r satellite
-                
-        
-print('mock.txt written')
-
-with open('nz.txt', 'w') as f:
-    for i in range(len(nbar)):
-        f.write('%e %e %e\n' % (nbar.z[i], nbar.nbar_obs[i], nbar.nbar_hod[i]))
+# Column 4: vr [km/s]
+# Column 5: redshift
+# Column 6: RA
+# Column 7: Dec
+# Column 8: M_host_halo
+# Column 9: r_satellite
+# Column 10: vr_satellite
