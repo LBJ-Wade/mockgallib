f229fa81b52f53031507afa0fa4c4e694c791a14
Jun Koda <jun.koda@brera.inaf.it>
2016-12-02 16:35:37 +0100

    Fix outdated notebook/mass_function.

--- py/mockgallib/mf.py
+++ py/mockgallib/mf.py
@@ -18,12 +18,12 @@ class MassFunction:
         return "MassFunction"
 
     def dndlnM(self, M, z):
-        sigma0 = self.s(M)
+        sigma0 = 1.0/mockgallib.sigma.sigma_0inv(M) #self.s(M)
         a =1.0/(1.0 + z)
         if a != self.a:
             c._mf_set_redshift(self._mf, a)
             self.a = a
-            self._D = growth_D(a)
+            self._D = mockgallib.growth_D(a)
         
         nu= self.delta_c/(self._D*sigma0)
         return c._mf_f(self._mf, nu)*self.rho_m/M
--- py/mockgallib/sigma.py
+++ py/mockgallib/sigma.py
@@ -15,7 +15,7 @@ M = None
 sinv = None
 
 
-def init(*, M_min=1.0e10, M_max=1.0e16, n=1001):
+def init(*, M_min=1.0e10, M_max=1.0e16, n_ =1001):
     """Initilises sigma module
 
     Optional keyword arguments:
@@ -23,9 +23,20 @@ def init(*, M_min=1.0e10, M_max=1.0e16, n=1001):
        M_max = 1.0e16: interpolation range maximum of sigma(M)
        n = 1001: number of interpolation points
     """
-
+    global n, M, sinv
+    n = n_
+    
     c._sigma_init(M_min, M_max, n)
 
     n = c._sigma_n()
     M = c._sigma_M_array()
     sinv = c._sigma_sinv_array()
+
+def sigma_0inv(M):
+    """
+    Arg:
+      M (float): Mass in [1/h M_solar]
+    Returns:
+      1/sigma0(M)
+    """
+    return c._sigma_0inv(M)
